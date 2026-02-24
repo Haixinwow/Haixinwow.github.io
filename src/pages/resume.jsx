cb447615a71453ea5b4f8f01b4db30bc5e42ee5a
Haixinwow <haixin@wustl.edu>
2026-02-23 20:35:46 -0600

    fill in more of the pages

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,6 +1,12 @@
 const Resume = () => {
     return(
-        <div>RESUME</div>
+        <div className="content">
+            <div style={{ display: 'flex', flexDirection: 'column', margin: 'auto'}}>
+                <h1 style={{ margin: 'auto' }}>ARCHITECTURE</h1>
+                <h4 style={{ margin: 'auto' }}>2019-2023 Architecture works</h4>
+            </div>
+            <iframe src="./assets/Resume_CS.pdf" width="100%" height="100%"></iframe>
+        </div>
     );
 }
 
